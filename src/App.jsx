59c6c23f3104c0fdb8d631453433d014610b859f
KhantNyi <khantnyithu@gmail.com>
2026-01-30 13:28:59 +0700

    CRUD assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { useEffect, useState } from 'react'; 
-import './App.css'
-function App() { 
- const [message, setMessage] = useState("...Loading..."); 
- async function fetchData() { 
- const result = await fetch('http://localhost:3000/api/hello'); 
- const data = await result.json(); 
- console.log("result: ", result); 
- console.log("data:", data); 
- setMessage(data.message); 
- } 
- useEffect(()=>{ 
- fetchData(); 
- },[]); 
- return ( 
- <div>
- Message: {message}
- </div>
- ) 
-} 
-export default App
+import { Routes, Route } from "react-router-dom";
+import { Items } from "./Items";
+import { ItemDetail } from "./ItemDetail";
+import TestApi from "./TestApi";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Items />} />
+      <Route path="/items" element={<Items />} />      
+      <Route path="/items/:id" element={<ItemDetail />} />
+      
+      {/* Existing test route */}
+      <Route path="/test_api" element={<TestApi />} />
+    </Routes>
+  );
+}
+
+export default App;
